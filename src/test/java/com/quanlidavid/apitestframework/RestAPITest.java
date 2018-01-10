88c87f6d25570425c77f4c17ca6d738572b5df2b
QuanLiDavid <quanlidavid@gmail.com>
2018-01-10 20:47:59 +0800

    init project

---
+++ src/test/java/com/quanlidavid/apitestframework/RestAPITest.java
@@ -0,0 +1,16 @@
+package com.quanlidavid.apitestframework;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postJira() {
+		System.out.println("postJira Test.");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("Delete Twitter test");
+	}
+}
